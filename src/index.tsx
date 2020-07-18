c99bf5846bd04c534566dd2320c352178c290050
Samanimkr <Samanimkr@Outlook.com>
2020-07-18 20:18:07 +0100

    set up the foundations

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => (
+    <div>
+        <h1>Hello!</h1>
+    </div>
+);
+
+ReactDOM.render(<Index />, document.getElementById('root'));
